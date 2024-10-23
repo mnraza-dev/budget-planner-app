dd19888d4663a5f033d43025bffeb6cf30be9fec
MN Raza <mnraza94@gmail.com>
2024-10-23 17:51:21 +0530

    started working on login screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import services from '../utils/services'
+import { useRouter } from 'expo-router'
+const Home = () => {
 
-const index = () => {
+  const router = useRouter();
+
+
+  useEffect(() => {
+    checkUserAuth()
+  }, [])
+  /**
+   * Check if user is logged in
+   */
+  const checkUserAuth = async () => {
+    const result = await services.getData('login')
+    if (result !== 'true') {
+      router.replace('/login')
+    }
+
+
+  }
   return (
     <View>
-      <Text>index</Text>
+      <Text>Home Screen </Text>
     </View>
   )
 }
 
-export default index
+export default Home
 
 const styles = StyleSheet.create({})
